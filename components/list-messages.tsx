dbdb1bb182d565b27b3ebe0b93e702d3d954bd83
Schlumen <lumen.haendler@web.de>
2025-03-02 18:00:45 +0100

    Scrolling

--- components/list-messages.tsx
+++ components/list-messages.tsx
@@ -3,13 +3,16 @@
 import { Imessage, useMessage } from "@/lib/store/messages";
 import Message from "./message";
 import { DeleteAlert, EditAlert } from "./message-actions";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { toast } from "sonner";
+import { ArrowDown } from "lucide-react";
 
 export default function ListMessages() {
   const scrollRef = useRef<HTMLDivElement>(null);
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [userScrolled, setUserScrolled] = useState(false);
+  const [notification, setNotification] = useState(0);
   const {
     messages,
     addMessage,
@@ -18,6 +21,17 @@ export default function ListMessages() {
     optimisticUpdateMessage,
   } = useMessage(state => state);
   const supabase = supabaseBrowser();
+
+  const scrollContainer = scrollRef.current;
+
+  const scrollDown = useCallback(() => {
+    if (!scrollContainer) return;
+    scrollContainer.scrollTo({
+      top: scrollContainer.scrollHeight,
+      behavior: "smooth",
+    });
+    setNotification(0);
+  }, [scrollContainer]);
 
   useEffect(() => {
     const channel = supabase
@@ -43,6 +57,14 @@ export default function ListMessages() {
             };
 
             addMessage(newMessage as Imessage);
+
+            if (!scrollContainer) return;
+            const isScroll =
+              scrollContainer.scrollTop <
+              scrollContainer.scrollHeight - scrollContainer.clientHeight - 150;
+            if (isScroll) {
+              setNotification(prev => prev + 1);
+            }
           }
         }
       )
@@ -72,28 +94,42 @@ export default function ListMessages() {
     optimisticIds,
     optimisticDeleteMessage,
     optimisticUpdateMessage,
+    scrollContainer,
   ]);
 
   useEffect(() => {
-    const scrollContainer = scrollRef.current;
-
-    if (scrollContainer && messages.length > 0) {
+    if (scrollContainer && messages.length > 0 && !userScrolled) {
       if (hasLoaded) {
-        scrollContainer.scrollTo({
-          top: scrollContainer.scrollHeight,
-          behavior: "smooth",
-        });
+        scrollDown();
       } else {
         scrollContainer.scrollTop = scrollContainer.scrollHeight;
         setHasLoaded(true);
       }
     }
-  }, [messages, hasLoaded]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messages, hasLoaded, scrollContainer]);
+
+  const handleOnScroll = () => {
+    if (scrollContainer) {
+      const isScroll =
+        scrollContainer.scrollTop <
+        scrollContainer.scrollHeight - scrollContainer.clientHeight - 150;
+      setUserScrolled(isScroll);
+
+      if (
+        scrollContainer.scrollTop ===
+        scrollContainer.scrollHeight - scrollContainer.clientHeight
+      ) {
+        setNotification(0);
+      }
+    }
+  };
 
   return (
     <div
       className="flex-1 flex flex-col p-5 h-full overflow-y-auto"
       ref={scrollRef}
+      onScroll={handleOnScroll}
     >
       <div className="flex-1"></div>
       <div className="space-y-5">
@@ -101,6 +137,27 @@ export default function ListMessages() {
           <Message key={index} message={value} />
         ))}
       </div>
+      {userScrolled && (
+        <div className="absolute bottom-20 left-0 right-0">
+          {notification ? (
+            <div
+              className="w-fit bg-primary py-1 px-2 rounded-md cursor-pointer hover:scale-110 transition-all mx-auto"
+              onClick={scrollDown}
+            >
+              <p>{`${notification} new message${
+                notification > 1 ? "s" : ""
+              }`}</p>
+            </div>
+          ) : (
+            <div
+              className="w-10 h-10 bg-primary rounded-full mx-auto flex items-center justify-center border cursor-pointer hover:scale-110 transition-all"
+              onClick={scrollDown}
+            >
+              <ArrowDown />
+            </div>
+          )}
+        </div>
+      )}
       <DeleteAlert />
       <EditAlert />
     </div>
